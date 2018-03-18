411765dae9ceab2ea38427c8216df7f7327ae8f3
Isabel Suchanek <isuchanek@gmail.com>
2018-03-17 18:56:32 -0700

    implement min and max methods in queue implemented using an list of fixed size

--- queue_arr.py
+++ queue_arr.py
@@ -39,7 +39,7 @@ class QueueArray(object):
             raise IndexError("enqueue to full queue")
 
         self._lst[self._back] = value
-        self._back += 1
+        self._back = (self._back + 1) % MAX_SIZE
 
     def dequeue(self):
         """Remove item from the queue. Raise error if queue is empty."""
@@ -71,7 +71,63 @@ class QueueArray(object):
         return self._lst[self._front]
 
     def min(self):
-        pass
+        if self.empty():
+            return None
+
+        current_min = self._lst[self._front]
+
+        # check if need to iterate over end of queue and start at beginning
+        if self._front < self._back:
+            for idx in range(self._front + 1, self._back):
+                current_min = self.__update_min(idx, current_min)
+        else:
+            # iterate from front to end, then idx 0 to back
+            for idx in range(self._front + 1, MAX_SIZE):
+                current_min = self.__update_min(idx, current_min)
+            for idx in range(0, self._back):
+                current_min = self.__update_min(idx, current_min)
+
+        return current_min
+
+    def __update_min(self, idx, current_min):
+        """
+        Private function to update current min if value at given index is less
+        than current_min.
+
+        Returns current_min.
+        """
+        if self._lst[idx] < current_min:
+            current_min = self._lst[idx]
+
+        return current_min
 
     def max(self):
-        pass
+        if self.empty():
+            return None
+
+        current_max = self._lst[self._front]
+
+        # check if need to cycle through queue or not
+        if self._front < self._back:
+            for idx in range(self._front, self._back):
+                current_max = self.__update_max(idx, current_max)
+        else:
+            # iterate front to end, 0 to back
+            for idx in range(self._front, MAX_SIZE):
+                current_max = self.__update_max(idx, current_max)
+            for idx in range(0, self._back):
+                current_max = self.__update_max(idx, current_max)
+
+        return current_max
+
+    def __update_max(self, idx, current_max):
+        """
+        Private function to update current_max if value at given index is less
+        than current_max.
+
+        Returns current_max.
+        """
+        if self._lst[idx] > current_max:
+            current_max = self._lst[idx]
+
+        return current_max
